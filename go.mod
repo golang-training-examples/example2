c38a106963c33be43adc2f3657b2d380b5cbe752
Ondrej Sika <ondrej@ondrejsika.com>
2023-05-31 10:12:06 +0200

    feat: Add hello packages

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/golang-training-examples/example2
 
 go 1.20
+
+require (
+	github.com/ondrejsika/go-hello v0.1.0 // indirect
+	gitlab.sikalabs.com/go/hello-from-gitlab v0.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ondrejsika/go-hello"
+	gitlab_hello "gitlab.sikalabs.com/go/hello-from-gitlab"
+)
 
 func main() {
+	hello.Hello()
+	gitlab_hello.HelloFromGitlab()
 	fmt.Println("Hello World")
 }
