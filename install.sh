c489834b669149bbf1ee060847b106f61af64927
Ondrej Sika <ondrej@ondrejsika.com>
2023-06-01 11:02:47 +0200

    feat(install.sh): Add linux install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+VERSION=v0.7.0 && \
+OS=linux && \
+ARCH=amd64 && \
+OWNER=golang-training-examples && \
+BIN=example2 && \
+curl -fsSL https://github.com/${OWNER}/${BIN}/releases/download/${VERSION}/${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz -o ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz && \
+tar -xzf ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz ${BIN} && \
+rm ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz && \
+mv ${BIN} /usr/local/bin/
