e99ebd22b2c6015cd54af8d1d88b34f43c879d82
Ondrej Sika <ondrej@ondrejsika.com>
2023-06-01 09:40:50 +0200

    feat: Add server port config using viper

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -2,8 +2,19 @@ package root
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var Cmd = &cobra.Command{
 	Use: "example2",
+}
+
+func init() {
+	viper.AddConfigPath("/etc/example2/")
+	viper.AddConfigPath("$HOME/example2/")
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.SetEnvPrefix("EXAMPLE2")
+	viper.ReadInConfig()
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,17 +4,34 @@ import (
 	"github.com/golang-training-examples/example2/cmd/root"
 	"github.com/golang-training-examples/example2/pkg/server"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var FlagPort int
 
 var Cmd = &cobra.Command{
 	Use:     "server",
 	Short:   "Run HTTP server",
 	Aliases: []string{"s"},
 	Run: func(c *cobra.Command, args []string) {
-		server.Server(8000)
+		server.Server(FlagPort)
 	},
 }
 
 func init() {
 	root.Cmd.AddCommand(Cmd)
+
+	viper.BindEnv("PORT")
+	port := viper.GetInt("PORT")
+
+	Cmd.Flags().IntVarP(
+		&FlagPort,
+		"port",
+		"p",
+		port,
+		"Listen on port",
+	)
+	if port == 0 {
+		Cmd.MarkFlagRequired("port")
+	}
 }
